b74118b0a6ef71650d738d23416d1b299182e1e6
Arthur Kafrouni Tomazi <arthur.k.t@hotmail.com>
2023-06-23 16:54:30 -0300

    Add files via upload

---
+++ lista1.1.3.py
@@ -0,0 +1,23 @@
+"""
+Lista 1, parte 1, exercício 3
+
+Descrição: Este programa apresenta a resolução de uma questão da lista 1 da cadeira de Economia Computacional
+Autor:Arthur Kafrouni Tomazi
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+# Atribuição de variáveis
+
+x=int(input("Primeiro número: "))
+y=int(input("Segundo número: "))
+
+# Entrada de dados
+
+
+# Processamento de dados
+
+
+# Saída de dados
+
+print("Soma dos números:",x+y)
